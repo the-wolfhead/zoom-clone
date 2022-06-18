3ba83452a902ca929109f55e4c754bef50091e1f
Daniel Otabor <danieldamianotabor@gmail.com>
2022-06-18 12:59:59 +0100

    created and fixed bugs for join-meeting link

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
+const bodyParser = require('body-parser');
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -19,6 +23,12 @@ app.get('/new:room', (req, res)=> {
     res.render('room', { roomId: req.params.room})
 })
 
+app.post('/join', (req,res)=> {
+    var id = req.body.ide;
+    console.log(id);
+    res.render('room', { roomId: id })   
+})  
+
 io.on('connection', socket => {
     socket.on('join-room', (roomId, userId) => {
         socket.join(roomId)
@@ -27,7 +37,7 @@ io.on('connection', socket => {
         socket.on('disconnect', () => {
             socket.to(roomId).emit('user-disconnected', userId)
         })
-    })
+    }) 
 })
 
 server.listen(process.env.PORT || 3000);
